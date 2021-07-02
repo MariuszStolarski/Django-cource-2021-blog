6855970a864dd2a4df94d3b5df96c501cb0e23e7
Mariusz Stolarski <mast@PL004611.local>
2021-07-02 15:16:54 +0200

    Adding html templates

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # view function for the "../" request
 def starting_page(request):
-    pass
+    return render(request, "blog/index.html")
 
 
 # view functionfor the "../posts" request
