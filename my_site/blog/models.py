af88cc83d2ed6504e4995083282cf7cc60447cef
mast <mariusz.stolarski@mobica.com>
2021-07-21 09:17:23 +0200

    Creating first table in DB and migrating

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    # blueprint of the model - database tables
+    # requires to run migration comment to register the model:
+    # python3 manage.py makemigrations
+    # and then:
+    # python3 manage.py migrate
+    title = models.CharField(max_length=50)
+
